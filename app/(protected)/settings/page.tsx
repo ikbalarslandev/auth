7d2dc29abe4d70508f2bf2c7011c77c7e8c9b70b
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-01-12 20:20:59 +0300

    adding callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,6 +2,8 @@ import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
   const session = await auth();
+
+  session?.user;
   return (
     <div>
       {JSON.stringify(session)}{" "}
