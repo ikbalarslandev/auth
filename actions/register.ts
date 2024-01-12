fd115b1431e2a442a78f78c31ac69a6a83b6a76a
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-01-13 01:03:43 +0300

    Verification token created

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import { RegisterSchema } from "@/schemas";
 import bcryptjs from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -32,7 +33,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
+  const verificationToken = await generateVerificationToken(email);
+
   return {
-    success: "User created",
+    success: "Confirmation email sent ",
   };
 };
